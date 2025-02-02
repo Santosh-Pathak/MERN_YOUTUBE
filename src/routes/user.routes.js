12cfde75bf6c9d48b28b66cd86c6074ab88ca266
Santosh Pathak <106902202+Santosh-Pathak@users.noreply.github.com>
2025-02-03 00:00:38 +0530

    Add routes for comments, likes, playlists, subscriptions, tweets, and videos with JWT authentication middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,32 +1,58 @@
 import { Router } from "express";
-import { loginUser , registerUser , logoutUser,refreshAccessToken } from "../controllers/user.controller.js";
-import { upload } from "../middlewares/multer.middleware.js";
-const router = Router();
+import { 
+    loginUser, 
+    logoutUser, 
+    registerUser, 
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateUserAvatar, 
+    updateUserCoverImage, 
+    getUserChannelProfile, 
+    getWatchHistory, 
+    updateAccountDetails
+} from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+
+const router = Router()
 
 router.route("/register").post(
-  upload.fields([
-    {
-      name: "avatar",
-      maxCount: 1,
-    },
-    {
-        name  : "coverImage",
-        maxCount: 1,
-    },
-  ]),
-  registerUser
-);
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        }, 
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+    )
 
-router.route("/login").post(loginUser);
-//SECURED ROUTER
-router.route("/logout").post(verifyJWT,logoutUser);
-router.route("/refresh-token").post(refreshAccessToken);
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT,  logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
+
+export default router;
 // here verifyJWT is  a middlwware which is used to validate the user ans succcessfully logout the user 
 // in middlewares (next) is used with req,res ? because here we have seeen that verifyJWt is used before logoutUser 
 // to verify that the user is Valid
 // next batata h ki pehle verifyJWT run karo and then logoutUser Run karo
 
-export default router;
 
 // Article for differencebetween accces token and refresh token
 
