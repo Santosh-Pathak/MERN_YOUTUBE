be7c9210abbe1f3708f9452ea3beafe27c7a624b
Santosh <pathaksantosh987@gmail.com>
2026-03-23 14:16:01 +0530

    Implement video comments with 1-level replies

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -11,6 +11,12 @@ const commentSchema = new Schema(
             type: Schema.Types.ObjectId,
             ref: "Video"
         },
+        // If set, this comment is a 1-level reply to another comment.
+        replyTo: {
+            type: Schema.Types.ObjectId,
+            ref: "Comment",
+            default: null,
+        },
         owner: {
             type: Schema.Types.ObjectId,
             ref: "User"
@@ -23,5 +29,7 @@ const commentSchema = new Schema(
 
 
 commentSchema.plugin(mongooseAggregatePaginate)
+
+commentSchema.index({ video: 1, replyTo: 1, createdAt: -1 });
 
 export const Comment = mongoose.model("Comment", commentSchema)
